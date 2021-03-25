818f9fd79881adfec26ab3342f962f401ab66ee9
Your Name <epyon85@gmail.com>
2021-03-25 19:54:24 -0400

    Redux part 1

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,24 @@
 import './HomeScreen.css';
 import Product from '../components/Product';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts as listProducts } from '../redux/actions/ProductActions';
 
 const HomeScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, loading, error } = getProducts
+
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch]);
+
     return <div className="homescreen">
        <h2 className="homes__title">Current Products</h2> 
 
        <div className="home__products">
-           <Product />
-           <Product />
-           <Product />
-           <Product />
            <Product />
            <Product />
            <Product />
